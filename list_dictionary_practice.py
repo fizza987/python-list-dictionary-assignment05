06d791452293849e34a00e04299461e2cd1041b6
Fizza Batool <batoolfizz704@gmail.com>
2025-05-12 00:45:07 +0500

    Add files via upload

---
+++ list_dictionary_practice.py
@@ -0,0 +1,34 @@
+##List
+
+fruits =["Mango","Banana","Strawberry"]
+
+#Display all items in the list
+print("Fruits list:", fruits)
+
+#Access a specific item in the list
+print("Second fruit:", fruits[1])
+
+#Add a new item to the list
+fruits.append("Peach")
+
+#Display the updated list 
+print("Updated fruits list:", fruits)
+
+##Dictionary
+
+student = {
+  "Name"  : "Fizza",
+  "Age"   :  "20",
+  "Grade" :  "12th "
+}
+#Display the entire dictionary
+print("Student Info:", student)
+
+#Access a specific value 
+print("Name:",student ["Name"])
+
+#Add a new key-value pair
+student["School"] = "Blessing Home High School"
+
+#Display the updated dictionary
+print("Updated Student Info:", student)
